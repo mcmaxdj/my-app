680dd0f99e5b13f960a684d57f26c298cc43992c
Maksym Sobetskyi <mcmaxdj@gmail.com>
2023-08-29 17:34:18 +0200

    add new user functionality

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -18,12 +18,28 @@ class App extends Component {
                 {name: 'Serg V.', salary: 2500, increase: false, id: 3},
             ]
         }
+        this.maxId = 4;
     }
 
     deleteItem = (id) => {
         this.setState(({data}) => {
             return {
                 data: data.filter(item => item.id !== id)
+            }
+        })
+    }
+
+    addItem = (name, salary) => {
+        const newItem = {
+            name,
+            salary,
+            increase: false,
+            id: this.maxId++
+        }
+        this.setState(({data}) => {
+            const newArr = [...data, newItem];
+            return {
+                data: newArr
             }
         })
     }
@@ -41,7 +57,8 @@ class App extends Component {
                 <EmployeesList 
                     data={this.state.data}
                     onDelete={this.deleteItem} />
-                <EmployeesAddForm/>
+                <EmployeesAddForm
+                    onAdd={this.addItem} />
             </div>
         );
     }
